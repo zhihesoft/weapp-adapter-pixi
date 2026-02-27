ad0e274684226c44c3ef677a5bec57c6a48321a4
stanley <houyuqing@hotmail.com>
2026-02-27 16:35:27 +0800

    add pack ignore

---
+++ build/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, '../src/index'),
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: 'weapp-adapter-pixi.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel', 'eslint-loader'],
+      },
+    ],
+  },
+}
